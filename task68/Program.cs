4a6f2c7a06f4f3e0e62b04d18ceaf775bdb7c1eb
Valeriy Fateev <Valerka9602@mail.ru>
2023-03-12 00:05:59 +0800

    Решение задачи 68

--- task68/Program.cs
+++ task68/Program.cs
@@ -7,3 +7,29 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 
 *******************************************************/
+
+
+int[] ReadInt(string text) // Ввод значений через пробел
+{
+    Console.Write(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+
+int Ackermann(int m, int n) // Функция Аккермана
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+    if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(m, n - 1));
+    return -1;
+}
+
+
+
+Console.WriteLine();
+int[] numbers = ReadInt("Введите неотрицательные m и n функции Аккермана через пробел: ");
+
+Console.WriteLine();
+if (numbers[0] < 0 || numbers[1] < 0) Console.WriteLine("\t" + "Указаны неверные значения m и n!");
+
+else Console.WriteLine("\t" + $"m = {numbers[0]}, n = {numbers[1]} —> A(m,n) = {Ackermann(numbers[0], numbers[1])}");
+Console.WriteLine();
